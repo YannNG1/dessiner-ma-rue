ca5c6231d80d307268102bbb6a4dcfd9f8986a9a
YannNG1 <93992399+YannNG1@users.noreply.github.com>
2022-01-04 13:39:46 +0100

    Add files via upload

---
+++ sol.py
@@ -0,0 +1,22 @@
+# Dépendances
+from ma_rue import rue, affiche 
+from trait import trait 
+
+# Définitions
+
+# Fonction sol()
+def sol():
+    '''
+    Trace une ligne horizontale au niveau du sol de la rue
+    d'épaisseur 3 pixels et de longueur 760 pixels
+    centrée dans le canvas
+            
+    '''
+    y_sol = rue.height-1 # ordonnée du sol de la rue
+    trait(40,y_sol,760,y_sol)
+    rue.line_width=3
+
+if __name__ == '__main__':
+
+	affiche(rue)
+	sol()
